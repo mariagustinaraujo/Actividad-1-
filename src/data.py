00e7250c8dfe83d64e492db3e2a2f5fb8254433b
Maria Araujo <mariaaraujo231199@gmail.com>
2025-09-17 15:40:30 -0300

    agrego evaluciones de las 5 rondas en data.py

--- src/data.py
+++ src/data.py
@@ -1,1 +1,42 @@
-
+evaluaciones = [
+    # Ronda 1
+    {
+        'EquipoA': {'innovacion': 2, 'presentacion': 1, 'errores': True},
+        'EquipoB': {'innovacion': 1, 'presentacion': 0, 'errores': False},
+        'EquipoC': {'innovacion': 1, 'presentacion': 2, 'errores': True},
+        'EquipoD': {'innovacion': 0, 'presentacion': 1, 'errores': False},
+        'EquipoE': {'innovacion': 1, 'presentacion': 1, 'errores': False}
+    },
+    # Ronda 2
+    {
+        'EquipoA': {'innovacion': 0, 'presentacion': 2, 'errores': False},
+        'EquipoB': {'innovacion': 2, 'presentacion': 0, 'errores': True},
+        'EquipoC': {'innovacion': 1, 'presentacion': 1, 'errores': False},
+        'EquipoD': {'innovacion': 2, 'presentacion': 1, 'errores': True},
+        'EquipoE': {'innovacion': 0, 'presentacion': 2, 'errores': False}
+    },
+    # Ronda 3
+    {
+        'EquipoA': {'innovacion': 3, 'presentacion': 2, 'errores': False},
+        'EquipoB': {'innovacion': 0, 'presentacion': 1, 'errores': True},
+        'EquipoC': {'innovacion': 1, 'presentacion': 1, 'errores': True},
+        'EquipoD': {'innovacion': 2, 'presentacion': 2, 'errores': False},
+        'EquipoE': {'innovacion': 2, 'presentacion': 2, 'errores': False}
+    },
+    # Ronda 4
+    {
+        'EquipoA': {'innovacion': 0, 'presentacion': 2, 'errores': True},
+        'EquipoB': {'innovacion': 1, 'presentacion': 1, 'errores': False},
+        'EquipoC': {'innovacion': 2, 'presentacion': 1, 'errores': False},
+        'EquipoD': {'innovacion': 2, 'presentacion': 2, 'errores': True},
+        'EquipoE': {'innovacion': 1, 'presentacion': 2, 'errores': True}
+    },
+    # Ronda 5
+    {
+        'EquipoA': {'innovacion': 2, 'presentacion': 2, 'errores': False},
+        'EquipoB': {'innovacion': 1, 'presentacion': 1, 'errores': False},
+        'EquipoC': {'innovacion': 0, 'presentacion': 2, 'errores': False},
+        'EquipoD': {'innovacion': 2, 'presentacion': 2, 'errores': False},
+        'EquipoE': {'innovacion': 2, 'presentacion': 3, 'errores': True}
+    }
+]
